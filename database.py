bb726021693e10f81a312e31f00c20d59938c5fe
vladZ1nets <vlad.zinets@gmail.com>
2024-12-02 14:09:08 +0200

    Оновлення проекту: додано стовпець 'role' в таблицю users

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
-DATABASE_URL = 'sqlite:///db1._db'
+DATABASE_URL = 'sqlite:///db.db'
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
